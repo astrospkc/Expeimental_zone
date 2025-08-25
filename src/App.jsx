33daf058f81191b8d800f273cb9a3d7e1c2cce20
Punam <punamkumari399@gmail.com>
2025-08-25 08:54:25 +0530

    creating tic-tac-toe game

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import VoiceInput from './components/VoiceInput'
 import { Bear } from './components/Bear'
+import Game from './components/Game'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <span>voice to text- implementation</span>
         <span><VoiceInput /></span>
         <span><Bear /></span>
+        <span><Game /></span>
       </div>
     </>
   )
---
+++ src/store/gameStore.js
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+import { combine } from 'zustand/middleware'
+
+const useGameStore = create(
+  combine(
+    {
+      history: [Array(9).fill(null)],
+      currentMove: 0,
+    },
+    (set, get) => {
+      return {
+        setHistory: (nextHistory) => {
+          set((state) => ({
+            history:
+              typeof nextHistory === 'function'
+                ? nextHistory(state.history)
+                : nextHistory,
+          }))
+        },
+        setCurrentMove: (nextCurrentMove) => {
+          set((state) => ({
+            currentMove:
+              typeof nextCurrentMove === 'function'
+                ? nextCurrentMove(state.currentMove)
+                : nextCurrentMove,
+          }))
+        },
+      }
+    },
+  ),
+)
+
+export default useGameStore
